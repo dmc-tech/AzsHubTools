622bee32e37008e861b71e569aebdc4762b62964
Daniel McDermott <daniel.mcdermott@avanade.com>
2020-05-21 16:20:05 +0100

    get-azsProductStatus.ps1

---
+++ Operator/Get-AzsProductDeploymentStatus.ps1
@@ -0,0 +1,32 @@
+#Requires -Modules @{'ModuleName'='Azs.Deployment.Admin';'ModuleVersion'='0.1.0'}
+[CmdletBinding()]
+Param (
+    [Parameter()][ValidateSet('microsoft.eventhub','microsoft.iothub')]
+    [String]$productid='microsoft.eventhub'
+
+)
+
+
+function Get-AzsProductStatus {
+    Param(
+        [Parameter()][ValidateSet('microsoft.eventhub','microsoft.iothub')]
+        [String]$productid
+    )
+
+    $status = (Get-AzsProductDeployment -ProductId $productid).properties.status
+    if ($status){
+        while ($status -notlike '*succeeded') {
+            $interval = 10
+            $status = (Get-AzsProductDeployment -ProductId $productid).properties.status 
+            write-output ('Sleeping {0} seconds: {1} current provisioning state: {2}' -f $interval, $productid, $status )
+            start-sleep -Seconds $interval
+        }
+        write-output ('{0} last operation was successful. Current provisioning state: {1}' -f $productid, $status )
+    }
+    else {
+        write-output ('{0} not found. Check you have connected to the admin environment and the RP is installed' -f $productid )
+
+    }
+}
+
+Get-AzsProductStatus $productid
