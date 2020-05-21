a40755fac7172e349951a81dcf05a1a0537d065b
Daniel McDermott <daniel.mcdermott@avanade.com>
2020-05-21 16:32:29 +0100

    added wait capability

--- Operator/Get-AzsProductDeploymentStatus.ps1
+++ Operator/Get-AzsProductDeploymentStatus.ps1
@@ -2,7 +2,8 @@
 [CmdletBinding()]
 Param (
     [Parameter()][ValidateSet('microsoft.eventhub','microsoft.iothub')]
-    [String]$productid='microsoft.eventhub'
+    [String]$productid='microsoft.eventhub',
+    [Switch]$wait
 
 )
 
@@ -15,13 +16,26 @@ function Get-AzsProductStatus {
 
     $status = (Get-AzsProductDeployment -ProductId $productid).properties.status
     if ($status){
-        while ($status -notlike '*succeeded') {
-            $interval = 10
-            $status = (Get-AzsProductDeployment -ProductId $productid).properties.status 
-            write-output ('Sleeping {0} seconds: {1} current provisioning state: {2}' -f $interval, $productid, $status )
-            start-sleep -Seconds $interval
+        if ($wait){
+            while ($status -notlike '*succeeded') {
+                $interval = 10
+                $status = (Get-AzsProductDeployment -ProductId $productid).properties.status 
+                write-output ('Sleeping {0} seconds: {1} current provisioning state: {2}' -f $interval, $productid, $status )
+                start-sleep -Seconds $interval
+            }
+            write-output ('{0} last operation was successful. Current status: {1}' -f $productid, $status )
         }
-        write-output ('{0} last operation was successful. Current provisioning state: {1}' -f $productid, $status )
+        else {
+            if ($status -notlike '*succeeded') {
+                write-output ('{0} current status: {1}' -f $productid, $status )
+            }
+            else {
+                write-output ('{0} last operation was successful. Current status: {1}' -f $productid, $status )
+
+            }
+
+        }
+        
     }
     else {
         write-output ('{0} not found. Check you have connected to the admin environment and the RP is installed' -f $productid )
